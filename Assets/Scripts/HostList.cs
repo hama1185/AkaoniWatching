fab6a3a33b194bcedf6265beafcd84a9d620769c
hama1185 <hamataku7332@softbank.ne.jp>
2019-12-16 17:37:54 +0900

    HostList add

--- Assets/Scripts/HostList.cs
+++ Assets/Scripts/HostList.cs
@@ -5,45 +5,84 @@ using UnityEngine;
 static public class HostList {
     static public Host phone1 = new Host(
         "192.168.11.58",
-        "status",
-        "flag",
-        "realsense",
-        8005,
-        8006,
+        "192.168.11.3",
+        8000,
+        8001,
         8002,
-        8000
+        8005,
+        8007,
+        8008
     );
     static public Host phone2 = new Host(
         "192.168.11.59",
-        "status",
-        "flag",
-        "realsense",
-        8006,
-        8005,
+        "192.168.11.4",
+        8001,
+        8000,
         8002,
-        8001
+        8006,
+        8007,
+        8008
     );
     static public string ip_audience = "192.168.11.60";
+    static public string ip_umpire = "192.168.11.24";
+
+    static public ID clientID = new ID(
+        "enemy",
+        "raspberry",
+        "audenece"
+    );
+    static public ServerName serverName = new ServerName(
+        "status",
+        "flag",
+        "realsence",
+        "umpire"
+    );
 
     public class Host {
         public string ip {get; set;}
-        public string server_status {get; set;}
-        public string server_flag {get; set;}
-        public string server_realsense {get; set;}
+        public string ip_raspberrypi {get; set;}
         public int port_status {get; set;}
         public int port_flag {get; set;}
         public int port_realsense {get; set;}
         public int port_audienceserver {get; set;}
+        public int port_raspberrypi {get; set;}
+        public int port_umpireReceive {get; set;}
 
-        public Host(string _ip, string server1, string server2, string server3, int port1, int port2, int port3, int port4) {
-            ip = _ip;
-            server_status = server1;
-            server_flag = server2;
-            server_realsense = server3;
+        public Host(string ip1, string ip2, int port1, int port2, int port3, int port4, int port5, int port6) {
+            ip = ip1;
+            ip_raspberrypi = ip2;
             port_status = port1;
             port_flag = port2;
             port_realsense = port3;
             port_audienceserver = port4;
+            port_raspberrypi = port5;
+            port_umpireReceive = port6;
+        }
+    }
+
+    public class ID {
+        public string enemy;
+        public string raspberrypi;
+        public string audience;
+
+        public ID(string id1, string id2, string id3) {
+            enemy = id1;
+            raspberrypi = id2;
+            audience = id3;
+        }
+    }
+
+    public class ServerName {
+        public string status;
+        public string flag;
+        public string realsense;
+        public string umpire;
+
+        public ServerName(string name1, string name2, string name3, string name4) {
+            status = name1;
+            flag = name2;
+            realsense = name3;
+            umpire = name4;
         }
     }
 }
