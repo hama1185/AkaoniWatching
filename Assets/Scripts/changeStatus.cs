f16a722f733360fb8244b5d34ecce19f43cf83d3
hama1185 <hamataku7332@softbank.ne.jp>
2020-02-12 17:43:07 +0900

    通信部の実装

---
+++ Assets/Scripts/changeStatus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeStatus : MonoBehaviour
+{
+    Slider villagerMindSlider;
+    Slider villagerRelaxSlider;
+    Slider ogreMindSlider;
+    Slider ogreRelaxSlider;
+    // Start is called before the first frame update
+    void Start(){
+        villagerMindSlider = GameObject.Find("Canvas").transform.Find("villagerMindSlider").GetComponent<Slider>();
+        villagerRelaxSlider = GameObject.Find("Canvas").transform.Find("villagerRelaxSlider").GetComponent<Slider>();
+        ogreMindSlider = GameObject.Find("Canvas").transform.Find("ogreMindSlider").GetComponent<Slider>();
+        ogreRelaxSlider = GameObject.Find("Canvas").transform.Find("ogreRelaxSlider").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        villagerMindSlider.value = VillagerStatus.mind;
+        villagerRelaxSlider.value = VillagerStatus.relax;
+        ogreMindSlider.value = OgreStatus.mind;
+        ogreRelaxSlider.value = OgreStatus.relax;
+    }
+}
